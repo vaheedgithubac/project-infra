7465c4f543b250e64414d05422a5c7514020e88b
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-21 18:56:18 +0530

    Create variables.tf

---
+++ 00-vpc/20-nat-instance/variables.tf
@@ -0,0 +1,17 @@
+
+variable "ami_id" {}
+variable "public_key_name" {}
+variable "remote_ip_to_connect_nat_instance" {}
+
+#############  Tags ###############################################
+variable "project_name" { default = "expense" }
+variable "env" { default = "dev" }
+
+variable "common_tags" {
+  default = {
+    Project     = "expense"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+###################################################################
