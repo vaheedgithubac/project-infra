0ad993fbe494ac95bd255dcd256acf221b030b9d
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-21 12:10:48 +0530

    Create outputs.tf

---
+++ 10-ami/outputs.tf
@@ -0,0 +1,17 @@
+############   outputs for  AL-2023  #####################
+output "ami_al_2023_free_tier_ami_id" {
+  value = module.ami_al_2023_free_tier.ami_id
+}
+
+output "ami_al_2023_free_tier_ami_name" {
+  value = module.ami_al_2023_free_tier.ami_name
+}
+
+############  outputs for  Amazon-Linux-2  #####################
+# output "ami_id" {
+#   value = module.ami_amazon-linux2_free_tier.ami_id
+# }
+
+# output "ami_name" {
+#   value = module.ami_amazon-linux2_free_tier.ami_name
+# }
