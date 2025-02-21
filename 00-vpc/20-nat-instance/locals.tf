bff79a40028986476c692d841348015998e384f8
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-21 19:04:22 +0530

    Delete 00-vpc/20-nat-instance directory

--- 00-vpc/20-nat-instance/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
-  # private_subnet_ids  = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
-  public_subnet_cidr  = split(",", data.aws_ssm_parameter.public_subnet_cidr.value)
-  private_subnet_cidr = split(",", data.aws_ssm_parameter.private_subnet_cidr.value)
-}
