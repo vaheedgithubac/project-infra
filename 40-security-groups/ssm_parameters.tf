c1f677ca193e288ec7061a7421efa2f4607713ed
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-21 21:16:27 +0530

    Update ssm_parameters.tf

--- 40-security-groups/ssm_parameters.tf
+++ 40-security-groups/ssm_parameters.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "mysql_sg" {
+resource "aws_ssm_parameter" "mysql_sg_id" {
   type  = "String"
-  name  = "/${var.project_name}/${var.env}/mysql_sg"
+  name  = "/${var.project_name}/${var.env}/mysql_sg_id"
   value = module.mysql_sg.sg_id
 }
