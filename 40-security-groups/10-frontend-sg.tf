4bb1db5158b81effc90f0cde5c84a518f000c4bd
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 17:45:19 +0530

    Update 10-frontend-sg.tf

--- 40-security-groups/10-frontend-sg.tf
+++ 40-security-groups/10-frontend-sg.tf
@@ -11,12 +11,12 @@ module "frontend_sg" {
 
 # Allowing traffic from web_alb --->  frontend instances
 resource "aws_security_group_rule" "frontend_web_alb" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
   source_security_group_id = module.web_alb_sg.sg_id
-  security_group_id = module.frontend_sg.sg_id
+  security_group_id        = module.frontend_sg.sg_id
 
-  depends_on = [ module.web_alb_sg ]
+  depends_on = [module.web_alb_sg]
 }
