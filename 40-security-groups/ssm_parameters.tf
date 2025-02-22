7b73b9b3a5e29f03fd87f05c52bfff5510255e61
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 13:39:55 +0530

    Update ssm_parameters.tf

--- 40-security-groups/ssm_parameters.tf
+++ 40-security-groups/ssm_parameters.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "mysql_sg_id" {
   name  = "/${var.project_name}/${var.env}/mysql_sg_id"
   value = module.mysql_sg.sg_id
 }
+
+resource "aws_ssm_parameter" "bastion_sg_sg_id" {
+  type  = "String"
+  name  = "/${var.project_name}/${var.env}/bastion_sg_sg_id"
+  value = module.bastion_sg.sg_id
+}
