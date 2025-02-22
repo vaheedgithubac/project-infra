efcec6642daea88f0579ef19b9b1610aab53515e
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 16:10:57 +0530

    Create 10-frontend-sg.tf

---
+++ 40-security-groups/10-frontend-sg.tf
@@ -0,0 +1,10 @@
+module "backend_sg" {
+  source         = "git::https://github.com/vaheedgithubac/Infra.git//modules/security_group"
+  vpc_id         = local.vpc_id
+  sg_name        = "frontend_sg"
+  sg_description = "Frontend Security Group"
+
+  project_name = var.project_name
+  env          = var.env
+  common_tags  = var.common_tags
+}
