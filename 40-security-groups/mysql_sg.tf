491eeb5f69ddf257dea7c71951e33eef6d0c6b96
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 13:39:30 +0530

    Update mysql_sg.tf

--- 40-security-groups/mysql_sg.tf
+++ 40-security-groups/mysql_sg.tf
@@ -18,7 +18,7 @@ resource "aws_security_group_rule" "mysql_bastion" {
   source_security_group_id = module.bastion_sg.sg_id
   security_group_id        = module.mysql_sg.sg_id
 
-  depends_on  = [ module.bastion_sg ]
+  depends_on = [module.bastion_sg]
 }
 
 # backend (private instances in private_subnet)  --->  mysql (database_subnet)
