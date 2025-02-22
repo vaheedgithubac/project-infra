cd7f45af6fced19b11391a377ec917e228f323be
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 16:35:38 +0530

    Update 30-backend-sg.tf

--- 40-security-groups/30-backend-sg.tf
+++ 40-security-groups/30-backend-sg.tf
@@ -17,4 +17,6 @@ resource "aws_security_group_rule" "backend_app_alb" {
   protocol          = "tcp"
   source_security_group_id = module.app_alb_sg.sg_id
   security_group_id = module.backend_sg.sg_id
+
+  depends_on = [ module.app_alb_sg ]
 }
