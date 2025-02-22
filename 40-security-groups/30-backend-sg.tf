304e2bb28e16b47c6257e73992fb69e862909e66
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 17:46:23 +0530

    Update 30-backend-sg.tf

--- 40-security-groups/30-backend-sg.tf
+++ 40-security-groups/30-backend-sg.tf
@@ -11,12 +11,12 @@ module "backend_sg" {
 
 # Allow traffic from app_alb ---> backend
 resource "aws_security_group_rule" "backend_app_alb" {
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
   source_security_group_id = module.app_alb_sg.sg_id
-  security_group_id = module.backend_sg.sg_id
+  security_group_id        = module.backend_sg.sg_id
 
-  depends_on = [ module.app_alb_sg ]
+  depends_on = [module.app_alb_sg]
 }
