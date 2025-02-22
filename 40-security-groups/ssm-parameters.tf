0cfac0249dcd6add25ef0050bcb36b6671d0d147
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 17:03:44 +0530

    Update ssm-parameters.tf

--- 40-security-groups/ssm-parameters.tf
+++ 40-security-groups/ssm-parameters.tf
@@ -1,11 +1,42 @@
+# web_alb_sg_id
+resource "aws_ssm_parameter" "web_alb_sg_id" {
+  type  = "String"
+  name  = "/${var.project_name}/${var.env}/web_alb_sg_id"
+  value = module.web_alb_sg.sg_id
+}
+
+# bastion_sg_sg_id
+resource "aws_ssm_parameter" "bastion_sg_sg_id" {
+  type  = "String"
+  name  = "/${var.project_name}/${var.env}/bastion_sg_sg_id"
+  value = module.bastion_sg.sg_id
+}
+
+# frontend_sg_id
+resource "aws_ssm_parameter" "frontend_sg_id" {
+  type  = "String"
+  name  = "/${var.project_name}/${var.env}/frontend_sg_id"
+  value = module.frontend_sg.sg_id
+}
+
+# app_alb_sg_id
+resource "aws_ssm_parameter" "app_alb_sg_id" {
+  type  = "String"
+  name  = "/${var.project_name}/${var.env}/app_alb_sg_id"
+  value = module.app_alb_sg.sg_id
+}
+
+# mysql_sg_id
 resource "aws_ssm_parameter" "mysql_sg_id" {
   type  = "String"
   name  = "/${var.project_name}/${var.env}/mysql_sg_id"
   value = module.mysql_sg.sg_id
 }
 
-resource "aws_ssm_parameter" "bastion_sg_sg_id" {
+# backend_sg_id
+resource "aws_ssm_parameter" "backend_sg_id" {
   type  = "String"
-  name  = "/${var.project_name}/${var.env}/bastion_sg_sg_id"
-  value = module.bastion_sg.sg_id
+  name  = "/${var.project_name}/${var.env}/backend_sg_id"
+  value = module.backend_sg.sg_id
 }
+
