4c3af95faa9d9db619f32ba04721e6794d6491ff
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 13:40:59 +0530

    Create variables.tf

---
+++ 40-security-groups/variables.tf
@@ -0,0 +1,12 @@
+#############################   Common Variables   #########################################
+variable "project_name" { default = "expense" }
+variable "env" { default = "dev" }
+
+variable "common_tags" {
+  default = {
+    Project     = "expense"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+#############################################################################################
