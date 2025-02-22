6a3d1a12a6c7b941fc3c74d5da9bd12f1362f1df
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-22 16:23:32 +0530

    Update 00-web-alb-sg.tf

--- 40-security-groups/00-web-alb-sg.tf
+++ 40-security-groups/00-web-alb-sg.tf
@@ -8,3 +8,21 @@ module "web_alb_sg" {
   env          = var.env
   common_tags  = var.common_tags
 }
+
+resource "aws_security_group_rule" "web_alb_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.web_alb_sg.sg_id
+}
+
+resource "aws_security_group_rule" "web_alb_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.web_alb_sg.sg_id
+}
