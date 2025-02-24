9e053e3cbc42ae6e61a23dfd471eff7c1d3ec1ec
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-24 19:10:42 +0530

    Update main.tf

--- 80-app-alb/main.tf
+++ 80-app-alb/main.tf
@@ -1,1 +1,102 @@
+# Create Private APP ALB
+module "alb" {
+  source = "git::https://github.com/vaheedgithubac/Infra//modules/alb"
 
+  internal              = true
+  alb_sg_ids            = [local.app_alb_sg_id]
+  subnets               = local.private_subnet_ids  
+  #vpc_id                = local.vpc_id
+
+  project_name = var.project_name
+  env          = var.env
+  common_tags  = var.common_tags
+}
+
+# create target group
+resource "aws_lb_target_group" "backend_app_alb_target_group" {
+  name = "${local.resource_name}-backend-app-alb-target-group"
+  # target_type = "ip"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+
+health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 5
+    matcher             = "200-299"
+    path                = "/"        # "/health"
+    port                = 8080
+    protocol            = "HTTP"
+    timeout             = 4
+  }
+
+# health_check {   AzeezSalu
+#   enabled             = true
+#   interval            = 300
+#   path                = "/"
+#   timeout             = 60
+#   matcher             = 200
+#   healthy_threshold   = 5
+#   unhealthy_threshold = 5
+# }
+
+lifecycle {
+  create_before_destroy = true
+  }
+}
+
+# create Listener for ALB
+resource "aws_lb_listener" "http" {
+  # count = var.internal ? 1 : 0
+
+  load_balancer_arn = module.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<center><h1>Hello, I am from backend APP ALB</h1></center>"
+      status_code  = "200"
+    }
+  }
+}
+
+# create a listener on port 80 with redirect action to 443 (http ---> https)
+# resource "aws_lb_listener" "alb_http_listener" {
+#   load_balancer_arn = aws_lb.application_load_balancer.arn
+#   port              = 80
+#   protocol          = "HTTP"
+
+#   default_action {
+#     type = "redirect"
+
+#     redirect {
+#       port        = 443
+#       protocol    = "HTTPS"
+#       status_code = "HTTP_301"
+#     }
+#   }
+# }
+
+# Create a Listener rule for ALB
+resource "aws_lb_listener_rule" "backend" {
+  listener_arn = aws_lb_listener.http.arn #local.app_alb_listener_arn
+  priority     = 100                      # low priority will be evaluated first
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_app_alb_target_group.arn   #aws_lb_target_group.backend.arn
+  }
+
+  condition {}  # empty condition block
+
+  #   condition {
+  #     host_header {
+  #       values = ["${var.backend_tags.Component}.app-${var.environment}.${var.zone_name}"]  # backend.app-dev.daws81s.online
+  #     }
+  #   }
+}
