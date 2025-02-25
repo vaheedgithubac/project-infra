fe57427745e9f60ce8df7626094704a1472a9b3e
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 16:45:13 +0530

    Update main.tf

--- 50-bastion-host/main.tf
+++ 50-bastion-host/main.tf
@@ -8,8 +8,10 @@ module "ec2" {
   subnet_id                   =  local.public_subnet_ids[0]    # "subnet-088e8443a70102e2a" #1a
   associate_public_ip_address = true
 
+  user_data                   = file("${path.module}/mysql_client_8.sh")
+
   # is_nat_instance             = var.is_nat_instance  # creates NAT instance if true
-  # user_data                   = file("${path.module}/mysql_client_8.sh")
+ 
 
   project_name                = var.project_name
   env                         = var.env
