e3b0742ca61c44909426b9aadf3f7def1c19bf17
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 13:39:21 +0530

    Create locals.tf

---
+++ 110-frontend-asg-ltmplt/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  sg_id                         = data.aws_ssm_parameter.frontend_sg_id.value
+  public_subnet_ids             = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
+  resource_name                 = "${var.project_name}-${var.environment}-frontend"
+  frontend_web_target_group_arn = data.aws_ssm_parameter.frontend_web_target_group_arn.value
+}
