b201a43f08b3537382722ad905af8d16a6efdcb5
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 14:00:11 +0530

    Update data.tf

--- 110-frontend-asg-ltmplt/data.tf
+++ 110-frontend-asg-ltmplt/data.tf
@@ -7,12 +7,6 @@ data "aws_ssm_parameter" "public_subnet_ids" {
   # depends_on = [ module.vpc ]
 }
 
-data "aws_ssm_parameter" "private_key" {
-  name            = "/frontend/ssh/private-key"
-  with_decryption = true
-  depends_on      = [ aws_ssm_parameter.private_key ]
-}
-
 data "aws_ssm_parameter" "frontend_app_target_group_arn" {
   name = "/${var.project_name}/${var.env}/frontend_app_target_group_arn"
 }
