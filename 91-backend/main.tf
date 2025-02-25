739e8a2302ebadd65c28d855c2d438dec212821c
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 11:33:53 +0530

    Update main.tf

--- 91-backend/main.tf
+++ 91-backend/main.tf
@@ -2,6 +2,7 @@ module "ec2" {
   source = "git::https://github.com/vaheedgithubac/Infra.git//modules/ec2"
 
   ami_id                      = var.ami_id
+  instance_type               = var.instance_type
   public_key_name             = var.public_key_name
   sg_id                       = [ local.sg_id ]
   subnet_id                   =  local.private_subnet_ids[0]    # "subnet-088e8443a70102e2a" #1a
