eed2ecb37b31861f6a6725852b2d106abcc4545a
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 11:43:45 +0530

    Update main.tf

--- 91-backend/main.tf
+++ 91-backend/main.tf
@@ -15,3 +15,38 @@ module "ec2" {
   env                         = var.env
   common_tags                 = var.common_tags
 }
+
+resource "null_resource" "backend" {
+  # Changes to any instance of the instance requires re-provisioning
+  triggers = {
+    instance_id = module.ec2.instance_id
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host        = module.ec2.ec2_instance.private_ip     #aws_instance.backend.private_ip
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = base64decode(data.aws_ssm_parameter.private_key.value)
+
+    # private_key = data.aws_ssm_parameter.private_key.value
+    # password = "DevOps321"
+  }
+
+  provisioner "file" {
+    source      = "backend.sh"
+    destination = "/tmp/backend.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "chmod +x /tmp/backend.sh",
+      "sudo sh /tmp/backend.sh backend dev"
+    ]
+  }
+  depends_on = [aws_ssm_parameter.private_key]
+}
+
+  
