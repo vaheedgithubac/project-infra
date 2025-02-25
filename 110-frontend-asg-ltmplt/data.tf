7ddaba1cd37c5b0d80aa8f589c5fb9391bdd5f1c
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 14:00:56 +0530

    Update data.tf

--- 110-frontend-asg-ltmplt/data.tf
+++ 110-frontend-asg-ltmplt/data.tf
@@ -7,6 +7,6 @@ data "aws_ssm_parameter" "public_subnet_ids" {
   # depends_on = [ module.vpc ]
 }
 
-data "aws_ssm_parameter" "frontend_app_target_group_arn" {
-  name = "/${var.project_name}/${var.env}/frontend_app_target_group_arn"
+data "aws_ssm_parameter" "frontend_web_target_group_arn" {
+  name = "/${var.project_name}/${var.env}/frontend_web_target_group_arn"
 }
