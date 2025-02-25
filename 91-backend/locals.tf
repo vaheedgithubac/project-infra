7298b66ba532b0029c6523f006cb79e9dea41f0d
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 11:31:38 +0530

    Update locals.tf

--- 91-backend/locals.tf
+++ 91-backend/locals.tf
@@ -1,4 +1,4 @@
 locals {
   sg_id                        = data.aws_ssm_parameter.backend_sg_id.value
-  private_subnet_ids           = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_ids           = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
 }
