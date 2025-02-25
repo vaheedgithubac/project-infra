ab574ca89a29a3c3f177bb74f06b1404ff6fe289
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 12:44:06 +0530

    Delete 90-app-asg directory

--- 90-app-asg/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  vpc_security_group_id        = data.aws_ssm_parameter.backend_sg_id.value
-  private_subnet_ids           = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
-  backend_app_target_group_arn = data.aws_ssm_parameter.backend_app_target_group_arn.value
-}
--- 90-app-asg/variable_values.tfvars
+++
@@ -1,3 +0,0 @@
-image_id        = "ami-0d682f26195e9ec0f"      # Base Image ID
-instance_type   = "t2.micro"
-key_name        = "mumbai-1"
