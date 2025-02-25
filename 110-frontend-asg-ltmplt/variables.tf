d91789e0983881f038045d02962c75faa227c34f
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 14:09:14 +0530

    Create variables.tf

---
+++ 110-frontend-asg-ltmplt/variables.tf
@@ -0,0 +1,16 @@
+#############################   Common Variables   ###############################################################
+variable "project_name" { default = "expense" }
+variable "env" { default = "dev" }
+
+variable "common_tags" {
+  default = {
+    Project     = "expense"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+##################################################################################################################
+variable "ami_id" {}
+variable "public_key_name" {}
+variable "instance_type" {}   # t2.micro / t2.small / t2.medium  only ( our ec2 module allows these 3 types only )
+##################################################################################################################
