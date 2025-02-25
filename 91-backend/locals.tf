d90c0ba484fb78339a4fd953a2cc3364a409d28d
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 12:00:24 +0530

    Update locals.tf

--- 91-backend/locals.tf
+++ 91-backend/locals.tf
@@ -1,4 +1,5 @@
 locals {
   sg_id                        = data.aws_ssm_parameter.backend_sg_id.value
   private_subnet_ids           = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  resource_name                = "${var.project_name}-${var.environment}-backend"
 }
