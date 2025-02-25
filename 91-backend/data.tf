81eb4052976769878e8c5eed3c9f8cd6466b5b2a
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 11:26:29 +0530

    Update data.tf

--- 91-backend/data.tf
+++ 91-backend/data.tf
@@ -1,3 +1,9 @@
 data "aws_ssm_parameter" "backend_sg_id" {
   name = "/${var.project_name}/${var.env}/backend_sg_id"
 }
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project_name}/${var.env}/private_subnet_ids"
+  # depends_on = [ module.vpc ]
+}
+
