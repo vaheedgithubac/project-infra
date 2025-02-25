0e99247c9a465489a9cc165f5413381068b68dcd
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-25 10:51:00 +0530

    Update main.tf

--- 90-app-asg/main.tf
+++ 90-app-asg/main.tf
@@ -1,1 +1,15 @@
+module "backend_app_asg_with_launch_template" {
+  source = ""
 
+  # Launch template variables
+  lt_type                = "backend_app"
+  image_id               = var.image_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [local.vpc_security_group_id]
+
+  # ASG variables
+  asg_type            = "backend_app"
+  target_group_arns   = [local.backend_app_target_group_arn]
+  vpc_zone_identifier = [local.private_subnet_ids]
+}
