46ff49243b07438108b0a7a5a2190c3dec3f8f44
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 09:03:17 +0530

    Update ssm-parameters.tf

--- 40-security-groups/ssm-parameters.tf
+++ 40-security-groups/ssm-parameters.tf
@@ -26,6 +26,13 @@ resource "aws_ssm_parameter" "app_alb_sg_id" {
   value = module.app_alb_sg.sg_id
 }
 
+# backend_sg_id
+resource "aws_ssm_parameter" "backend_sg_id" {
+  type  = "String"
+  name  = "/${var.project_name}/${var.env}/backend_sg_id"
+  value = module.backend_sg.sg_id
+}
+
 # mysql_sg_id
 resource "aws_ssm_parameter" "mysql_sg_id" {
   type  = "String"
@@ -33,9 +40,4 @@ resource "aws_ssm_parameter" "mysql_sg_id" {
   value = module.mysql_sg.sg_id
 }
 
-# backend_sg_id
-resource "aws_ssm_parameter" "backend_sg_id" {
-  type  = "String"
-  name  = "/${var.project_name}/${var.env}/backend_sg_id"
-  value = module.backend_sg.sg_id
-}
+
