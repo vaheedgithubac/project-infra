aa9bc2fe91f803b57debff614a3511b39f8cc8a1
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 18:56:52 +0530

    Update variables.tf

--- 90-backend-asg-ltmplt/variables.tf
+++ 90-backend-asg-ltmplt/variables.tf
@@ -12,5 +12,6 @@ variable "common_tags" {
 ##################################################################################################################
 variable "image_id" {}
 variable "public_key_name" {}
-variable "instance_type" {}   # t2.micro / t2.small / t2.medium  only ( our ec2 module allows these 3 types only )
+variable "instance_type" {} # t2.micro / t2.small / t2.medium  only ( our ec2 module allows these 3 types only )
 ##################################################################################################################
+
