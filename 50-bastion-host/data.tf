5ee0c1e2b3eccce3f8120cf57cb0fa695e5dd30b
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 19:23:31 +0530

    Update data.tf

--- 50-bastion-host/data.tf
+++ 50-bastion-host/data.tf
@@ -1,5 +1,5 @@
 data "aws_ssm_parameter" "bastion_sg_sg_id" {
-  name  = "/${var.project_name}/${var.env}/bastion_sg_sg_id"
+  name = "/${var.project_name}/${var.env}/bastion_sg_sg_id"
 }
 
 data "aws_ssm_parameter" "public_subnet_ids" {
