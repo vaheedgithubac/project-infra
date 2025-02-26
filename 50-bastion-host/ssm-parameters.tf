6284fea5b07f1b9147eaf86203d3fb659ef1964c
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 13:47:36 +0530

    Create ssm-parameters.tf

---
+++ 50-bastion-host/ssm-parameters.tf
@@ -0,0 +1,5 @@
+resource "aws_ssm_parameter" "bastion_host_public_ip" {
+  type  = "String"
+  name  = "/${var.project_name}/${var.env}/bastion_host_public_ip"
+  value = module.bastion_ec2.ec2_instance.public_ip
+}
