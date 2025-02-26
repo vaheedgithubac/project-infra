bbfc563945966a4319b89c8889bb642f6db38795
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 15:31:06 +0530

    Update main.tf

--- 90-backend-asg-ltmplt/main.tf
+++ 90-backend-asg-ltmplt/main.tf
@@ -12,4 +12,8 @@ module "backend_app_asg_with_launch_template" {
   asg_type            = "backend_app"
   target_group_arns   = [local.backend_app_target_group_arn]
   vpc_zone_identifier = [local.private_subnet_ids]
+
+  project_name = var.project_name
+  env          = var.env
+  common_tags  = var.common_tags
 }
