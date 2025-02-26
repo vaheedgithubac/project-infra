d1f7a2741619d8217ba681145ccd67dbcf62a366
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 19:24:28 +0530

    Update main.tf

--- 50-bastion-host/main.tf
+++ 50-bastion-host/main.tf
@@ -4,17 +4,18 @@ module "bastion_ec2" {
   ami_id                      = var.ami_id
   instance_type               = var.instance_type
   public_key_name             = var.public_key_name
-  sg_id                       = [ local.sg_id ]
-  subnet_id                   =  local.public_subnet_ids[0]    # "subnet-088e8443a70102e2a" #1a
+  sg_id                       = [local.sg_id]
+  subnet_id                   = local.public_subnet_ids[0] # "subnet-088e8443a70102e2a" #1a
   associate_public_ip_address = true
   what_type_instance          = "bastion"
 
-  user_data                   = file("${path.module}/mysql_client_8.sh")
+  user_data = file("${path.module}/mysql_client_8.sh")
 
   # is_nat_instance             = var.is_nat_instance  # creates NAT instance if true
- 
 
-  project_name                = var.project_name
-  env                         = var.env
-  common_tags                 = var.common_tags
+
+  project_name = var.project_name
+  env          = var.env
+  common_tags  = var.common_tags
 }
+
