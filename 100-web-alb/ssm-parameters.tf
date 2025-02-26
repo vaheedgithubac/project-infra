18bed3d1444027d14aca358422b4affcf511c87f
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 19:59:44 +0530

    Update ssm-parameters.tf

--- 100-web-alb/ssm-parameters.tf
+++ 100-web-alb/ssm-parameters.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "front_web_target_group_arn" {
+resource "aws_ssm_parameter" "frontend_web_target_group_arn" {
   type  = "String"
-  name  = "/${var.project_name}/${var.env}/front_web_target_group_arn"
+  name  = "/${var.project_name}/${var.env}/frontend_web_target_group_arn"
   value =  aws_lb_target_group.frontend_web_target_group.arn     
 }
