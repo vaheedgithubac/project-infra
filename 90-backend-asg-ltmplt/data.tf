584312981db02c59838811896f1a2edd8439f12f
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 13:48:53 +0530

    Update data.tf

--- 90-backend-asg-ltmplt/data.tf
+++ 90-backend-asg-ltmplt/data.tf
@@ -16,3 +16,7 @@ data "aws_ssm_parameter" "private_key" {
 data "aws_ssm_parameter" "backend_app_target_group_arn" {
   name = "/${var.project_name}/${var.env}/backend_app_target_group_arn"
 }
+
+data "aws_ssm_parameter" "bastion_host_public_ip" {
+  name = "/${var.project_name}/${var.env}/bastion_host_public_ip"
+}
