88b0cdcd9da230fdab4045b9df5e95d1d3dee9f4
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 14:25:22 +0530

    Update locals.tf

--- 90-backend-asg-ltmplt/locals.tf
+++ 90-backend-asg-ltmplt/locals.tf
@@ -1,7 +1,7 @@
 locals {
   sg_id                        = data.aws_ssm_parameter.backend_sg_id.value
   private_subnet_ids           = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
-  resource_name                = "${var.project_name}-${var.environment}-backend"
+  resource_name                = "${var.project_name}-${var.env}-backend"
   backend_app_target_group_arn = data.aws_ssm_parameter.backend_app_target_group_arn.value
   bastion_host_public_ip       = data.aws_ssm_parameter.bastion_host_public_ip.value"
 }
