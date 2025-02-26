48588b46456e1da5547a8c00a890d5b469dc09d0
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 09:01:16 +0530

    Update 30-backend-sg.tf

--- 40-security-groups/30-backend-sg.tf
+++ 40-security-groups/30-backend-sg.tf
@@ -21,11 +21,11 @@ resource "aws_security_group_rule" "backend_app_alb" {
   depends_on = [module.app_alb_sg]
 }
 
-# bastion (public_subnet) ---> backend (private_subnet)
+# bastion 22 (public_subnet) ---> backend (private_subnet)
 resource "aws_security_group_rule" "backend_bastion" {
   type                     = "ingress"
-  from_port                = 3306
-  to_port                  = 3306
+  from_port                = 22
+  to_port                  = 22
   protocol                 = "tcp"
   source_security_group_id = module.bastion_sg.sg_id
   security_group_id        = module.backend_sg.sg_id
