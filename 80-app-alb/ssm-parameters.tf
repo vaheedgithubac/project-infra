00725d9771e023babc398dd44d7e3cf39908cd5f
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 14:20:12 +0530

    Update ssm-parameters.tf

--- 80-app-alb/ssm-parameters.tf
+++ 80-app-alb/ssm-parameters.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "backend_app_target_group_arn" {
   type  = "String"
   name  = "/${var.project_name}/${var.env}/backend_app_target_group_arn"
-  value = aws_lb_target_group.backend_app_alb_target_group.arn
+  value = aws_lb_target_group.backend_app_target_group.arn
 }
 
