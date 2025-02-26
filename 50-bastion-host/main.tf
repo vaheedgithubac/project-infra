c7b24eef6fb7e78986d517d7e66dcaf3a650efa7
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-26 10:35:42 +0530

    Update main.tf

--- 50-bastion-host/main.tf
+++ 50-bastion-host/main.tf
@@ -1,4 +1,4 @@
-module "ec2" {
+module "bastion_ec2" {
   source = "git::https://github.com/vaheedgithubac/Infra.git//modules/ec2"
 
   ami_id                      = var.ami_id
@@ -7,6 +7,7 @@ module "ec2" {
   sg_id                       = [ local.sg_id ]
   subnet_id                   =  local.public_subnet_ids[0]    # "subnet-088e8443a70102e2a" #1a
   associate_public_ip_address = true
+  what_type_instance          = var.what_type_instance
 
   user_data                   = file("${path.module}/mysql_client_8.sh")
 
