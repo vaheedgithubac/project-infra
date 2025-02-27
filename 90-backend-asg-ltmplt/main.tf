d0c96ba8caaecde5b5c9943883a87ea8c9aae6ca
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-27 16:56:11 +0530

    Update main.tf

--- 90-backend-asg-ltmplt/main.tf
+++ 90-backend-asg-ltmplt/main.tf
@@ -9,10 +9,17 @@ module "backend_app_asg_with_launch_template" {
   vpc_security_group_ids = [local.sg_id]
 
   # ASG variables
+  min_size                  = 1
+  max_size                  = 2
+  desired_capacity          = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+
   asg_type            = "backend_app"
   target_group_arns   = [local.backend_app_target_group_arn]
   vpc_zone_identifier = local.private_subnet_ids
 
+  # Tags
   project_name = var.project_name
   env          = var.env
   common_tags  = var.common_tags
