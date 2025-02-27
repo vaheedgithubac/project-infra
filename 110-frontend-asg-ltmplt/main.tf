1078e0fa007db9e00cfbe2eaab8ec1a83eb34c45
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-27 17:03:56 +0530

    Update main.tf

--- 110-frontend-asg-ltmplt/main.tf
+++ 110-frontend-asg-ltmplt/main.tf
@@ -8,6 +8,8 @@ module "frontend_web_asg_with_launch_template" {
   key_name               = var.public_key_name
   vpc_security_group_ids = [local.sg_id]
 
+  associate_public_ip_address = true
+
   user_data              = file("${path.module}/frontend-dev-user-data.sh")
 
   # ASG variables
