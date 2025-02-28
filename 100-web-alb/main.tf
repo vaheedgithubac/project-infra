9ff9abede35266a940545fee309bc0f62c06cbea
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-02-28 10:11:59 +0530

    Update main.tf

--- 100-web-alb/main.tf
+++ 100-web-alb/main.tf
@@ -19,6 +19,7 @@ resource "aws_lb_target_group" "frontend_web_target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = local.vpc_id
+  deregistration_delay = 60
 
 health_check {
     enabled             = true
