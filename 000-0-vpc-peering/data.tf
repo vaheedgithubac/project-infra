5a2d39b89abb26525a6bcbe575e4d69277c1a9f5
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-06 18:18:53 +0530

    Update data.tf

--- 000-0-vpc-peering/data.tf
+++ 000-0-vpc-peering/data.tf
@@ -1,4 +1,9 @@
-data "aws_ssm_parameter" "vpc_id" {
+data "aws_ssm_parameter" "accepter_vpc_id" {
   name = "/expense/dev/vpc_id"
+  # name = "/${var.project_name}/${var.env}/vpc_id"
+}
+
+data "aws_ssm_parameter" "accepter_vpc_cidr" {
+  name = "/expense/dev/vpc_cidr"
   # name = "/${var.project_name}/${var.env}/vpc_id"
 }
