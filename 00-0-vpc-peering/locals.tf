fbfe26cd1a284b7e0badb112aa890566eca56261
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-06 13:20:27 +0530

    Create locals.tf

---
+++ 00-0-vpc-peering/locals.tf
@@ -0,0 +1,6 @@
+/*
+locals {
+  requester_vpc_route_table_ids = tolist(toset(data.aws_route_tables.requester.ids))
+  accepter_vpc_route_table_ids = tolist(toset(data.aws_route_tables.accepter.ids))
+}
+*/
