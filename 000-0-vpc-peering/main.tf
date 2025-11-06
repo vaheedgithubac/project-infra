238a6add8fc8ec6cf2047714224d2a0ec070cf55
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-06 17:33:51 +0530

    Update main.tf

--- 000-0-vpc-peering/main.tf
+++ 000-0-vpc-peering/main.tf
@@ -1,6 +1,6 @@
 ############### If you want 1 az, 1 public subnet, 1 private subnet and 1 database subnet ########################
  module "vpc-org" {
-   source = "git::https://github.com/vaheedgithubac/Infra//modules/vpc" # Give the path to VPC MODULE accordingly
+   source = "git::https://github.com/vaheedgithubac/Infra//modules/vpc"          # Give the path to VPC MODULE accordingly
 
    # All the counts should be same 
    azs_count             = 1
@@ -13,9 +13,9 @@
    private_subnet_cidr  = ["10.200.11.0/24"]
    database_subnet_cidr = ["10.200.31.0/24"]
 
-   project_name = "org"   # var.project_name    
-   env          = "dev"   # var.env             
-   common_tags  = var.common_tags
+   project_name =  var.project_name    # "org"
+   env          =  var.env             # "dev"
+   common_tags  =  var.common_tags
  }
 
 module "vpc-peering" {
