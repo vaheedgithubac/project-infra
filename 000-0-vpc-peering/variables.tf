df7a285227b26f432c550dba648e0b91fec694a3
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-06 18:30:31 +0530

    Update variables.tf

--- 000-0-vpc-peering/variables.tf
+++ 000-0-vpc-peering/variables.tf
@@ -1,6 +1,10 @@
 variable "region" {}
-variable "project_name" { default = "org" }
-variable "env" { default = "dev" }
+
+variable "requester_project_name" {}
+variable "accepter_project_name" {}
+
+variable "requester_project_env" {}
+variable "accepter_project_env" {}
 
 variable "common_tags" {
   default = {
