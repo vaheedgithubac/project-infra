1e60d386d12a4621515fc9598595cb85943b928b
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 15:07:29 +0530

    Update variables.tf

--- 000-0-vpc-peering/variables.tf
+++ 000-0-vpc-peering/variables.tf
@@ -6,6 +6,9 @@ variable "accepter_project_name" {}
 variable "requester_project_env" {}
 variable "accepter_project_env" {}
 
+variable "requester_route_table_ids" { type = list }
+variable "accepter_route_table_ids" { type = list }
+
 variable "common_tags" {
   default = {
     Project     = "org"
