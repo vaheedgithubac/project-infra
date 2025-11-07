faeb90606d967a143172f0e0eb010d73d6c89da7
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 20:17:24 +0530

    Update data.tf

--- 000-1-vpc-peering/data.tf
+++ 000-1-vpc-peering/data.tf
@@ -1,1 +1,11 @@
+# Fetch the requester VPC ID
+data "aws_ssm_parameter" "requester_vpc_id" {
+  name = "/${var.requester_project_name}/${var.requester_project_env}/vpc_id"
+}
+
+# Fetch the accepter VPC ID
+data "aws_ssm_parameter" "accepter_vpc_id" {
+  name = "/${var.accepter_project_name}/${var.accepter_project_env}/vpc_id"
+}
+
 
