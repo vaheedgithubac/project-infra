e170862b7a043d1455e1f4d04c1fb81d3aea5a4c
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 20:09:06 +0530

    Update variables.tf

--- 000-0-vpc-2/variables.tf
+++ 000-0-vpc-2/variables.tf
@@ -1,4 +1,11 @@
 variable "region" {}
+
+variable "requester_project_name" {}
+variable "accepter_project_name" {}
+
+variable "requester_project_env" {}
+variable "accepter_project_env" {}
+
 variable "common_tags" {
   default = {
     Project     = "org"
