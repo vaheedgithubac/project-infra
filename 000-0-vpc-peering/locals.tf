10574bd5f72457addfcd1837b14949961e17c0ec
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 12:19:26 +0530

    Update locals.tf

--- 000-0-vpc-peering/locals.tf
+++ 000-0-vpc-peering/locals.tf
@@ -1,3 +1,10 @@
 locals {
-  accepter_vpc_id   = data.aws_ssm_parameter.accepter_vpc_id.value
+  requester_vpc_id   = module.vpc-org.vpc_id
+  accepter_vpc_id    = data.aws_ssm_parameter.accepter_vpc_id.value
+
+  requester_vpc_cidr = data.aws_vpc.requester.cidr_block
+  accepter_vpc_cidr  = data.aws_vpc.accepter.cidr_block
+
+  requester_route_table_ids  =  
+  accepter_route_table_ids  =
 }
