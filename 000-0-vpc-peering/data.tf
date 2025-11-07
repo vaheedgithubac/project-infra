71b6ce8bd0f3e5bbe7a75ebe51f19b62a9bc9aa9
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 12:20:09 +0530

    Update data.tf

--- 000-0-vpc-peering/data.tf
+++ 000-0-vpc-peering/data.tf
@@ -3,4 +3,36 @@ data "aws_ssm_parameter" "accepter_vpc_id" {
   name = "/${var.accepter_project_name}/${var.accepter_project_env}/vpc_id"
 }
 
+###############################################################################
+# DATA SOURCES: Fetch VPC Details (like cidr_block and many more)
+###############################################################################
+
+data "aws_vpc" "requester" {
+  id = var.requester_vpc_id
+}
+
+data "aws_vpc" "accepter" {
+  id = var.accepter_vpc_id
+}
+
+###############################################################################
+# DATA SOURCES: Fetch all route tables in a given VPC
+###############################################################################
+
+data "aws_route_tables" "requester" {
+  # vpc_id = var.requester_vpc_id
+  filter {
+    name   = "vpc-id"
+    values = [var.requester_vpc_id]
+  }
+}
+
+data "aws_route_tables" "accepter" {
+  # vpc_id = var.accepter_vpc_id
+  filter {
+    name   = "vpc-id"
+    values = [var.accepter_vpc_id]
+  }
+}
+
 
