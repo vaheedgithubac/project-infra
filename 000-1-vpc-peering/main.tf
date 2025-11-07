f23440b01c318c226e63904afd2fdf480ca28cd8
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-11-07 20:13:39 +0530

    Create main.tf

---
+++ 000-1-vpc-peering/main.tf
@@ -0,0 +1,7 @@
+module "vpc-peering" {
+  source = "git::https://github.com/vaheedgithubac/Infra//modules/vpc_peering"   # Give the path to VPC MODULE accordingly
+
+  region                       =  var.region
+  requester_vpc_id             =  local.requester_vpc_id                       # module.vpc-org.vpc_id                        # "/org/dev/vpc_id"
+  accepter_vpc_id              =  local.accepter_vpc_id                        # "/expense/dev/vpc_id"      
+}
